c3a393d064487e5a17d54d2c880c3e64d14a37db
James <jb7301722@gmail.com>
2016-03-23 19:46:12 -0500

    add test for properties controller

---
+++ test/controllers/properties_controller_test.rb
@@ -0,0 +1,109 @@
+require 'test_helper'
+
+class PropertiesControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
+  test "get index" do
+    sign_in :user, users(:one)
+    get :index
+    assert_template :index
+    assert_response :success
+    assert_not_nil assigns(:properties)
+  end
+
+  test "index redirect to signin and display error if not logged in" do
+    get :index
+    assert_redirected_to new_user_session_path
+  end
+
+  test "test new" do
+    sign_in :user, users(:one)
+    get :new
+    assert_response :success
+    assert_not_nil assigns(:property)
+  end
+
+  test "new should redirect to signin if no current user" do
+    get :new
+    assert_redirected_to new_user_session_path
+  end
+
+  test "create should successfully create property" do
+    sign_in :user, users(:one)
+
+    assert_difference 'Property.count', 1 do
+      post :create, property: {
+        address: "Foo1",
+        city: "Blah",
+        state: "AL",
+        zip: "23423",
+        deposit: 300,
+        rent: 500,
+      }
+    end
+
+    assert_redirected_to property_path assigns(:property)
+    assert_equal 'Property successfully saved', flash[:success]
+  end
+
+  test "create should redirect to signin if no current user" do
+    post :create
+    assert_redirected_to new_user_session_path
+  end
+
+  test "show should show properties details page" do
+    sign_in :user, users(:one)
+
+    get :show, id: properties(:one)
+    assert_response :success
+    assert_template :show
+    assert_not_nil assigns(:property)
+  end
+
+  test "show should redirect to signin if no current user" do
+    get :show, id: properties(:one)
+    assert_redirected_to new_user_session_path
+  end
+
+  test "get edit" do
+    sign_in :user, users(:one)
+
+    get :edit, id: properties(:one)
+    assert_response :success
+    assert_template :edit
+    assert_not_nil assigns(:property)
+  end
+
+  test "edit should redirect to signin if no current user" do
+    get :edit, id: properties(:one)
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should be able to update property" do
+    sign_in :user, users(:one)
+
+    put :update, id: properties(:one), property: {
+      address: "NewAddress",
+      city: "NewCity",
+      state: "CA",
+      zip: "511",
+      deposit: 100,
+      rent: 200
+    }
+
+    assert_equal "NewAddress", assigns(:property).address
+    assert_equal "NewCity", assigns(:property).city
+    assert_equal "CA", assigns(:property).state
+    assert_equal "511", assigns(:property).zip
+    assert_equal 100, assigns(:property).deposit
+    assert_equal 200, assigns(:property).rent
+
+    assert_equal 'Property successfully updated', flash[:success]
+  end
+
+  test "should not be able to update property if not logged in" do
+    put :update, id: properties(:one)
+    assert_redirected_to new_user_session_path
+  end
+
+end
