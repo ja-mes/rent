2c9c83a577e0f87d61335b28d9e47475ba7035de
James <jb7301722@gmail.com>
2016-03-27 12:48:35 -0500

    add rented to property

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160327165011) do
+ActiveRecord::Schema.define(version: 20160327174757) do
 
   create_table "customers", force: :cascade do |t|
     t.string  "first_name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160327165011) do
     t.decimal "deposit"
     t.integer "user_id"
     t.integer "customer_id"
+    t.string  "rented"
   end
 
   create_table "users", force: :cascade do |t|
