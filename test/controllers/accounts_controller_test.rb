3793d10713552fad4e7da34b2be48bd183737f8c
James <jb7301722@gmail.com>
2016-04-26 19:56:55 -0500

    add accounts tests

--- test/controllers/accounts_controller_test.rb
+++ test/controllers/accounts_controller_test.rb
@@ -1,16 +1,112 @@
 class AccountsControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
-#  test "get index" do
-#    sign_in :users, users(:one)
-#    get :index
-#    assert_response :success
-#    assert_template :index
-#    assert_not_nil assigns(:accounts)
-#  end
-#
-#  test "get index should not work if user is not logged in" do
-#    get :index
-#    assert_redirected_to new_user_session_path
-#  end
+  test "get index" do
+    sign_in :user, users(:one)
+
+    get :index
+    assert_response :success
+    assert_template :index
+    assert_not_nil assigns(:accounts)
+  end
+
+  test "get index should not work if user is not logged in" do
+    get :index
+    assert_redirected_to new_user_session_path
+  end
+
+  test "get new" do
+    sign_in :user, users(:one)
+
+    get :new
+    assert_response :success
+    assert_not_nil assigns(:account)
+  end
+
+  test "get new should not work if the user is not logged in" do
+    get :new
+    assert_redirected_to new_user_session_path
+  end
+
+  test "post create" do
+    sign_in :user, users(:one)
+
+    assert_difference 'Account.count' do
+      post :create, account: {
+        name: "Foo"
+      }
+    end
+
+    assert_not_nil assigns(:account)
+    assert_not_nil assigns(:account).user
+  end
+
+  test "post create shoud fail with invalid data" do
+    sign_in :user, users(:one)
+
+    assert_difference 'Account.count', 0 do
+      post :create, account: {
+        name: ""
+      }
+    end
+  end
+
+  test "post create should not work if user is not logged in" do
+    post :create, account: {}
+
+    assert_redirected_to new_user_session_path
+  end
+
+  test "get show" do
+    sign_in :user, users(:one)
+
+    get :show, id: accounts(:one)
+    assert_response :success
+    assert_template :show
+    assert_not_nil assigns(:account)
+  end
+
+  test "get show should not work if user is not logged in" do
+    get :show, id: accounts(:one)
+    assert_redirected_to new_user_session_path
+  end
+
+  test "get edit" do
+    sign_in :user, users(:one)
+
+    get :edit, id: accounts(:one)
+    assert_response :success
+    assert_template :edit
+    assert_not_nil assigns(:account)
+  end
+
+  test "get edit should not work if the user is not signed in" do
+    get :edit, id: accounts(:one)
+    assert_redirected_to new_user_session_path
+  end
+
+  test "post update" do
+    sign_in :user, users(:one)
+
+    post :update, id: accounts(:one), account: {
+      name: "Bar"
+    }
+
+    assert_equal assigns(:account).name, "Bar"
+  end
+
+  test "post update should not work if invalid form data is submitted" do
+    sign_in :user, users(:one)
+
+    post :update, id: accounts(:one), account: {
+      name: ""
+    }
+
+    assert_template :edit
+  end
+
+  test "post update should not work if user is not logged in" do
+    post :update, id: accounts(:one), account: {}
+    assert_redirected_to new_user_session_path
+  end
 end
--- test/models/accounts_test.rb
+++ test/models/accounts_test.rb
@@ -2,4 +2,19 @@ class AccountsTest < ActiveSupport::TestCase
   test "account should be valid" do
     assert accounts(:one).valid?
   end 
+
+  test "user_id should be present" do
+    accounts(:one).user = nil
+    assert_not accounts(:one).valid?
+  end
+
+  test "name should be present" do
+    accounts(:one).name = "  "
+    assert_not accounts(:one).valid?
+  end
+
+  test "balance should be present" do
+    accounts(:one).balance = nil
+    assert_not accounts(:one).valid?
+  end
 end
