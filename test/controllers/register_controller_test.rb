4eb85d1bd04305a866bb200ff4995d3130b44e7a
James <jb7301722@gmail.com>
2016-04-29 19:26:04 -0500

    Add tests for account trans

--- test/controllers/register_controller_test.rb
+++ test/controllers/register_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class RegisterControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::TestHelpers
+  
+  # GET index
+  test "get index" do
+    sign_in :user, users(:one)
+
+    get :index
+    assert_not_nil assigns(:trans)
+    assert_not_nil assigns(:balance)
+  end
+
+  test "get index should not work if the user is not logged in" do
+    get :index
+    assert_redirected_to new_user_session_path
+  end
 end
--- test/models/account_tran_test.rb
+++ test/models/account_tran_test.rb
@@ -1,6 +1,29 @@
 require 'test_helper'
 
 class AccountTranTest < ActiveSupport::TestCase
-  test "invoice tran should be valid" do
+  def setup
+    @tran = account_trans(:one)
+  end
+
+  test "should belong to user" do
+    assert users(:one).account_trans
+  end
+
+  test "should belong to accounts" do
+    assert accounts(:one).account_trans
+  end
+
+  test "should belong to properties" do
+    assert properties(:one).account_trans
+  end
+
+  test "user id should be present" do
+    @tran.user = nil
+    assert_not @tran.valid?
+  end
+
+  test "amount should be present" do
+    @tran.amount = nil
+    assert_not @tran.valid?
   end
 end
