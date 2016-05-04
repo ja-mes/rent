2cba04ca8fc8d29a2cdb3d5c220916d931c20c49
James <jsbrown000@gmail.com>
2016-05-04 17:03:09 -0500

    Add basic tests for vendors

--- test/controllers/vendors_controller_test.rb
+++ test/controllers/vendors_controller_test.rb
@@ -1,7 +1,54 @@
 require 'test_helper'
 
 class VendorsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::TestHelpers
+
+  # GET index
+  test "get index" do
+    sign_in :user, users(:one)
+
+    get :index
+    assert_response :success
+    assert_template :index
+    assert_not_nil assigns(:vendors)
+  end
+
+  test "get index should not work if the user is not logged in" do
+    get :index
+    assert_redirected_to new_user_session_path
+  end
+
+
+  # GET new
+  test "get new" do
+    sign_in :user, users(:one)
+
+    get :new
+    assert_response :success
+    assert_not_nil assigns(:vendor)
+  end
+
+  test "get new should only work if the user is logged in" do
+    get :new
+    assert_redirected_to new_user_session_path
+  end
+
+
+  # POST create
+  test "post create should successfully create vendor" do
+    sign_in :user, users(:one)
+
+    assert_difference 'Vendor.count' do
+      post :create, vendor: {
+        name: "Foo Bar"
+      }
+    end
+
+    assert_equal assigns(:vendor).name, "Foo Bar"
+  end
+
+  test "post create should not work if the user is not logged in" do
+    post :create, vendor: {}
+    assert_redirected_to new_user_session_path
+  end
 end
--- test/models/vendor_test.rb
+++ test/models/vendor_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class VendorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @vendor = vendors(:one)
+  end
+
+  test "should be valid" do
+    assert @vendor.valid?
+  end
+
+  test "user should be present" do
+    @vendor.user_id = nil
+    assert_not @vendor.valid?
+  end
+
+  test "name should be present" do
+    @vendor.name = nil
+    assert_not @vendor.valid?
+  end
+
 end
