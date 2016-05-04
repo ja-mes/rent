0c0529cf3e9d02c69ea0ec9d6d7e299a65f95e2c
James <jsbrown000@gmail.com>
2016-05-04 18:17:00 -0500

    Sort register trans by date desc

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -2,7 +2,7 @@ class RegisterController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @trans = current_user.trans.where("transactionable_type = 'Deposit' OR transactionable_type = 'Check'")
+    @trans = current_user.trans.where("transactionable_type = 'Deposit' OR transactionable_type = 'Check'").order('date DESC')
     @balance = Account.find_by(user: current_user, name: "Checking").balance
   end
 end
