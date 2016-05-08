b296614c48450ce1a109330a4102d312dd5f75e1
James <jsbrown000@gmail.com>
2016-05-08 13:50:56 -0500

    Add tests for daterange on accounts. Closes #34

--- test/models/account_tran_test.rb
+++ test/models/account_tran_test.rb
@@ -31,4 +31,18 @@ class AccountTranTest < ActiveSupport::TestCase
     @tran.date = nil
     assert_not @tran.valid?
   end
+
+  test "calculate total should calculate balance" do
+    trans = AccountTran.where(amount: 9.99).limit(3)
+    assert_equal trans.calculate_total, 29.97
+  end
+
+  test "date range should return transactions for a date range" do
+   trans = AccountTran.all.limit(3).date_range(
+      {'date(1i)' => "2016", "date(2i)" => "7", "date(3i)" => "7" },
+      {'date(1i)' => "2016", "date(2i)" => "8", "date(3i)" => "7" }
+    )
+
+   assert_equal trans.count, 3
+  end
 end
