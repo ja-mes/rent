a42e5b7f8f218068397d2c7505a38f93970bcd89
James <jsbrown000@gmail.com>
2016-05-09 08:30:46 -0500

    Update balance when account trans are removed and update names. Fixes #51

--- app/assets/javascripts/account_trans.js
+++ app/assets/javascripts/account_trans.js
@@ -5,12 +5,34 @@ function add_fields(link, association, content) {
   $('table tr:last').after(content.replace(regexp, new_id));  
 }
 
-$(document).on("click", '.remove_account_tran', function() {
-  event.preventDefault();
-  $(this).prev().val("1");
-  $(this).closest('.account_tran_fields').hide();
-});
+;(function () {
 
-$(document).on('turbolinks:load', function() {
-  $('.remove_account_tran').prev().val("false");
-});
+  function calculateTotal() {
+    var total = 0;
+
+    $('.account_tran_amount').each(function(i, elem) {
+      if ($(elem).is(":visible")) {
+        total += Number($(elem).val().replace(/[^0-9\.]+/g,""));
+      }
+    });
+
+    $('#account_trans_total').html("$" + total.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,"));
+  }
+
+  $(document).on('turbolinks:load', function() {
+    calculateTotal();
+    $('.remove_account_tran').prev().val("false");
+  });
+
+  $(document).on('change', '.account_tran_amount', function() {
+    calculateTotal()
+  });
+
+  $(document).on("click", '.remove_account_tran', function() {
+    event.preventDefault();
+    $(this).prev().val("1");
+    $(this).closest('.account_tran_fields').hide();
+    calculateTotal()
+  });
+
+})();
