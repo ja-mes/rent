e2ddfb7503a9c5aa2aecaabc7149f8aed98075ba
James <jsbrown000@gmail.com>
2016-05-16 19:56:34 -0500

    Add account type test

--- app/models/account_type.rb
+++ app/models/account_type.rb
@@ -1,5 +1,7 @@
 class AccountType < ActiveRecord::Base
   belongs_to :user
   has_many :accounts
-
+  
+  validates :user_id, presence: true
+  validates :name, presence: true
 end
--- test/models/account_type_test.rb
+++ test/models/account_type_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class AccountTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @type = account_types(:income)
+  end
+
+  test "should be valid" do
+    assert @type.valid?
+  end
+
+  test "user should be present" do
+    @type.user = nil
+    assert_not @type.valid?
+  end
+
+  test "name should be present" do
+    @type.name = "   "
+    assert_not @type.valid?
+  end
 end
