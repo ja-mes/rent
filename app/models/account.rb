39d96bfcaa8140e5bba018c465c662cc22a11ff6
James <jsbrown000@gmail.com>
2016-05-18 19:07:42 -0500

    comments

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,10 +1,11 @@
 class Account < ActiveRecord::Base
-  # ASSOCIATIONs
+  # ASSOCIATIONS
   belongs_to :user
   belongs_to :account_type
   has_many :account_trans
   has_many :payments
 
+  # VALIDATIONS
   validates :user_id, presence: true
   validates :name, presence: true
   validates :balance, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ } 
