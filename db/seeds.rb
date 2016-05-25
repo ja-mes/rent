c1de0a0346fac8b910c2f3e40e557d2e56f30c41
James <jsbrown000@gmail.com>
2016-05-25 07:10:39 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 user = User.create!(email: "test@example.com", password: 'password', password_confirmation: 'password')
 
-100.times do
+20.times do
   property = Property.create!(user: user, address: "#{Faker::Address.street_address}  #{Faker::Address.street_suffix}", state: Faker::Address.state_abbr, city: Faker::Address.city, zip: Faker::Address.zip, rent: Faker::Commerce.price, deposit: Faker::Commerce.price)
   customer = Customer.create!(user: user, property: property, first_name: Faker::Name.first_name, middle_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone: Faker::PhoneNumber.cell_phone, alt_phone: Faker::PhoneNumber.phone_number, balance: 0, charged_today: false, rent: 500, due_date: "1", active: true)
 
-  10.times do
+  5.times do
     invoice = Invoice.new(user: user, customer: customer, amount: Faker::Commerce.price, date: Date.today, memo: Faker::Lorem.sentence)
     invoice.skip_tran_validation = true
     invoice.save
@@ -12,7 +12,7 @@ user = User.create!(email: "test@example.com", password: 'password', password_co
     AccountTran.create!(user: user, amount: invoice.amount, memo: invoice.memo, account: Account.find_by(name: "Rental Income"), property: Property.order("RANDOM()").first, date: invoice.date, account_transable: invoice)
   end
 
-  11.times do
+  6.times do
     Payment.create!(user: user, customer: customer, amount: Faker::Commerce.price, date: Date.today, memo: Faker::Lorem.sentence, account: Account.find_by(name: "Undeposited Funds"))
   end
 
