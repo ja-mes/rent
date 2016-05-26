29c8b54f24d40378042d35b334f07578690ac1dc
James <jsbrown000@gmail.com>
2016-05-26 14:18:37 -0500

    REmove example jobs

--- app/jobs/my_job.rb
+++
@@ -1,9 +0,0 @@
-class MyJob
-  @queue = :default
-
-  def self.perform
-    2.times do
-      Vendor.create(name: Faker::Name.name, user: User.first)
-    end
-  end
-end
--- app/jobs/test_job.rb
+++
@@ -1,7 +0,0 @@
-class TestJob
-  @queue = :default
-
-  def self.perform
-    Vendor.create(name: Faker::Name.name, user: User.first)
-  end
-end
