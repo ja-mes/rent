40744040abae27adb4dec6d6393aef6626099a9b
James <jsbrown000@gmail.com>
2016-05-30 18:20:32 -0500

    Submit recurring tran form with ajax

--- app/controllers/recurring_trans_controller.rb
+++ app/controllers/recurring_trans_controller.rb
@@ -3,6 +3,11 @@ class RecurringTransController < ApplicationController
     model = tran_params[:type].singularize.classify.constantize
     item = model.find_by(user: current_user, id: tran_params[:id])
     RecurringTran.memorize item, tran_params[:due_date]
+
+    respond_to do |f|
+      f.html
+      f.js
+    end
   end
 
   private
