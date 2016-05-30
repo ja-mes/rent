8fb06ca6e72ee63d66cef723a153014fc349f7dd
James <jsbrown000@gmail.com>
2016-05-30 14:41:35 -0500

    Pass id into controller

--- app/controllers/recurring_trans_controller.rb
+++ app/controllers/recurring_trans_controller.rb
@@ -1,5 +1,13 @@
 class RecurringTransController < ApplicationController
   def create
+    model = tran_params[:type].singularize.classify.constantize
+    item = model.find_by(user: current_user, id: tran_params[:id])
+
     debugger
+  end
+
+  private
+  def tran_params
+    params.permit(:id, :type, :due_date)
   end
 end
