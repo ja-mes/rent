7a7a7103b31b4ad9f32a485a40e1b129f94d49f4
James <jsbrown000@gmail.com>
2016-05-30 20:27:00 -0500

    Add recurring tran controller test

--- test/controllers/recurring_trans_controller_test.rb
+++ test/controllers/recurring_trans_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class RecurringTransControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::TestHelpers
+  
+  test "post create should work for checks" do
+    sign_in :user, users(:one)
+
+    assert_difference "RecurringTran.count" do
+      xhr :post, :create, id: checks(:one).id, type: "Check", due_date: "5"
+    end
+  end
 end
