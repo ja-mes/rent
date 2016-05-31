6625bc363b87adf7f491da5719463d7e80665052
James <jsbrown000@gmail.com>
2016-05-31 07:01:44 -0500

    Add test for enter recurring trans job

--- test/jobs/enter_recurring_trans_job_test.rb
+++ test/jobs/enter_recurring_trans_job_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class EnterRecurringTransJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should enter tran for checks" do
+    recurring_trans(:one).update_attribute(:due_date, Date.today.day.to_s)
+
+    assert_difference ['Check.count', 'AccountTran.count'] do
+      EnterRecurringTransJob.perform_now users(:one).id
+    end
+  end
 end
