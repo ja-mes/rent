b974429ba8c140c0e73ddf10bd102827e6512046
James <jsbrown000@gmail.com>
2016-06-03 18:59:39 -0500

    Fix charge rent job test

--- test/jobs/charge_rent_job_test.rb
+++ test/jobs/charge_rent_job_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class ChargeRentJobTest < ActiveJob::TestCase
   test "should charge rent" do
-    customers(:one).update_attribute(:due_date, Date.today.day.to_s)
-    customers(:three).update_attribute(:due_date, Date.today.day.to_s)
+    customers(:one).update_attribute(:last_charged, Date.today.prev_month)
+    customers(:three).update_attributes(last_charged: Date.today.prev_month, active: true)
 
     assert_difference ['Invoice.count', 'AccountTran.count', 'Tran.count'], 2 do
       ChargeRentJob.perform_now users(:one).id
