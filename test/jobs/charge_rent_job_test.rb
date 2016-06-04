70bb5f46fcaed2a55a7ff72c77c47b4b48a17db6
James <jsbrown000@gmail.com>
2016-06-03 19:01:26 -0500

    Test to ensure that inactive customers are not charged rent

--- test/jobs/charge_rent_job_test.rb
+++ test/jobs/charge_rent_job_test.rb
@@ -10,6 +10,15 @@ class ChargeRentJobTest < ActiveJob::TestCase
     end
   end
 
+  test "charge rent job should not work for inactive customers" do
+    customers(:one).update_attribute(:last_charged, Date.today)
+    customers(:three).update_attributes(last_charged: Date.today.prev_month)
+
+    assert_difference ['Invoice.count', 'AccountTran.count', 'Tran.count'], 0 do
+      ChargeRentJob.perform_now users(:one).id
+    end
+  end
+
   test "rent should be charged after user is logged in" do
     assert_enqueued_with(job: ChargeRentJob) do
       User.first.after_database_authentication
