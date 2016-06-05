df0adb894b3a7e37f0a083a75221ab8a6ddbbcbe
James <jsbrown000@gmail.com>
2016-06-04 19:25:09 -0500

    Update enter recurring trans job

--- test/jobs/enter_recurring_trans_job_test.rb
+++ test/jobs/enter_recurring_trans_job_test.rb
@@ -2,9 +2,18 @@ require 'test_helper'
 
 class EnterRecurringTransJobTest < ActiveJob::TestCase
   test "should enter tran for checks" do
-    recurring_trans(:one).update_attribute(:due_date, Date.today.day.to_s)
+    recurring_trans(:one).update_attribute(:last_charged, Date.today.prev_month)
 
     assert_difference ['Check.count', 'AccountTran.count'] do
+      EnterRecurringTransJob.perform_now users(:one).id
+    end
+  end
+
+  test "should not enter trans that have been entered in the last month" do
+    recurring_trans(:one).last_charged = Date.today.prev_month + 10
+
+    debugger
+    assert_difference ['Check.count', 'AccountTran.count'], 0 do
       EnterRecurringTransJob.perform_now users(:one).id
     end
   end
