8dfeb3c84352c0e2355c8ff60d85b5bdc887210f
James <jsbrown000@gmail.com>
2016-06-07 18:06:53 -0500

    Use register balance

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -3,6 +3,6 @@ class RegisterController < ApplicationController
 
   def index
     @trans = current_user.trans.where("transactionable_type = 'Deposit' OR transactionable_type = 'Check'").order('date DESC')
-    @balance = Account.find_by(user: current_user, name: "Checking").balance
+    @balance = Register.find_by(user: current_user, name: "Checking").balance
   end
 end
