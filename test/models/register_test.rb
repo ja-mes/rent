68eed84e3731be4e70e42158b68f03111c662096
James <jsbrown000@gmail.com>
2016-06-08 11:39:04 -0500

    Test register model

--- test/models/register_test.rb
+++ test/models/register_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class RegisterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @register = registers(:one)
+  end
+
+  test "should be valid" do
+    assert @register.valid?
+  end
+
+  test "user_id should be present" do
+    @register.user = nil
+    assert_not @register.valid?
+  end
+
+  test "balance should be present" do
+    @register.balance = nil
+    assert_not @register.valid?
+  end
+
+  test "name should be present" do
+    @register.name = "  "
+    assert_not @register.valid?
+  end
 end
