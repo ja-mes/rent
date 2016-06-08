e2c75448c69e8f5cbb76e7425454e122bc3934d0
James <jsbrown000@gmail.com>
2016-06-08 13:31:13 -0500

    Put reconciliation into form

--- app/controllers/reconciliations_controller.rb
+++ app/controllers/reconciliations_controller.rb
@@ -3,6 +3,7 @@ class ReconciliationsController < ApplicationController
   end
 
   def new
+    @reconciliation = Reconciliation.new
     @checks = Check.where(user: current_user, cleared: false).order('date DESC')
     @deposits = Deposit.where(user: current_user, cleared: false).order('date DESC')
     render layout: "fluid"
