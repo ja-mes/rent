4dbcebd84b7a732e966b197d0f4efcb6f83502a1
James <jsbrown000@gmail.com>
2016-06-08 12:20:56 -0500

    Create basic new template

--- app/controllers/reconciliations_controller.rb
+++ app/controllers/reconciliations_controller.rb
@@ -2,5 +2,17 @@ class ReconciliationsController < ApplicationController
   def index
   end
 
+  def new
+  end
+  
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
   private
 end
