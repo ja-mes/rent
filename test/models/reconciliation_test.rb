761f4cc56dca2729dd2efcf9db567f7e9a4e95b7
James <jsbrown000@gmail.com>
2016-06-12 12:56:39 -0500

    Add baisc validations tests

--- test/models/reconciliation_test.rb
+++ test/models/reconciliation_test.rb
@@ -8,4 +8,14 @@ class ReconciliationTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @rec.valid?
   end
+
+  test "user should be present" do
+    @rec.user = nil
+    assert_not @rec.valid?
+  end
+
+  test "ending_balance should be present" do
+    @rec.ending_balance = nil
+    assert_not @rec.valid?
+  end
 end
