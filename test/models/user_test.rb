47bfed852696f30ef8a95e36aff6c75c924d59b2
James <jsbrown000@gmail.com>
2016-06-15 12:43:47 -0500

    Test account methods in user model. Closes #116

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,6 +5,18 @@ class UserTest < ActiveSupport::TestCase
     @user = users(:one)
   end
 
+  def work_account(fixture, method_name)
+    assert_equal fixture, users(:one).send(method_name)
+
+    fixture.destroy
+
+    assert_difference "#{fixture.class.name}.count" do
+      users(:one).send(method_name)
+    end
+  end
+
+  #def work_accounts(fixture)
+
   test "vacant_properties should return vacant properties for the user" do
     assert_equal @user.vacant_properties.count, 1
   end
@@ -41,5 +53,55 @@ class UserTest < ActiveSupport::TestCase
     assert_difference 'Register.count' do
       users(:one).checkbook
     end
+  end
+
+  test "income_account_type" do
+    work_account(account_types(:income), 'income_account_type')
+  end
+
+  test "bank_account_type" do
+    work_account(account_types(:bank), 'bank_account_type')
+  end
+
+  test "other_current_assets_account_type" do
+    work_account(account_types(:other_current_assets), 'other_current_assets_account_type')
+  end
+
+  test "other_current_liabilities_account_type" do
+    work_account(account_types(:other_current_liabilities), 'other_current_liabilities_account_type')
+  end
+
+  test "other_income_account_type" do
+    work_account(account_types(:other_income), 'other_income_account_type')
+  end
+
+  test "expenses_account_type" do
+    work_account(account_types(:expenses), 'expenses_account_type')
+  end
+
+
+  # accounts
+  test "rental_income_account" do
+    work_account(accounts(:one), 'rental_income_account')
+  end
+
+  test "security_deposits_account" do
+    work_account(accounts(:three), 'security_deposits_account')
+  end
+
+  test "undeposited_funds_account" do
+    work_account(accounts(:five), 'undeposited_funds_account')
+  end
+
+  test "deposit_discrepancies_account" do
+    work_account(accounts(:six), 'deposit_discrepancies_account')
+  end
+
+  test "reconciliaton_discrepancies_account" do
+    work_account(accounts(:reconcile_discrepancies), 'reconciliaton_discrepancies_account')
+  end
+
+  test "repairs_and_maintenance_account" do
+    work_account(accounts(:repairs), 'repairs_and_maintenance_account')
   end
 end
