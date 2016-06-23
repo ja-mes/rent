9c7b94f3d7e3c6212f77a08f52a1f6b00a90fe30
James <jsbrown000@gmail.com>
2016-06-23 13:19:39 -0500

    Only run js if we are on customers new page

--- app/assets/javascripts/customers.js
+++ app/assets/javascripts/customers.js
@@ -1,33 +1,36 @@
 $(document).on('turbolinks:load', function() {
-  function format_currency(elem, amount) {
-    if ($(elem)[0]) {
-      $(elem).html('$' + amount.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,"));
+  if ($('.customers.new')) {
+    function format_currency(elem, amount) {
+      if ($(elem)[0]) {
+        $(elem).html('$' + amount.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,"));
+      }
     }
-  }
-
-  var date = new Date();
-  var yesterday = new Date(date.setDate(date.getDate() - 1));
-  var days_in_month = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
-
-  $('#customer_prorated_rent_checkbox').prop({ 'checked': true });
-  $('#customer_deposit_checkbox').prop({ 'checked': true });
-  $('#customer_days_in_month').html(days_in_month - yesterday.getDate());
-
-  $('#customer_rent').on('change', function() {
-    var rent_amount = +$(this).val();
 
     var date = new Date();
-    var yesterday = new Date(date.setDate(date.getDate() - 1))
-      var days_in_month = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
+    var yesterday = new Date(date.setDate(date.getDate() - 1));
+    var days_in_month = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
 
-    if (yesterday.getDate() != 1) {
-      rent_amount = +((rent_amount / days_in_month) * (days_in_month - yesterday.getDate())).toFixed(2)
-    }
+    $('#customer_prorated_rent_checkbox').prop({ 'checked': true });
+    $('#customer_deposit_checkbox').prop({ 'checked': true });
+    $('#customer_days_in_month').html(days_in_month - yesterday.getDate());
 
-    format_currency('#customer_prorated_rent_amount', rent_amount)
-  });
+    $('#customer_rent').on('change', function() {
+      var rent_amount = +$(this).val();
 
-  $('#customer_deposit').on('change', function() {
-    format_currency('#customer_deposit_amount', +$(this).val())
-  });
+      var date = new Date();
+      var yesterday = new Date(date.setDate(date.getDate() - 1))
+        var days_in_month = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
+
+      if (yesterday.getDate() != 1) {
+        rent_amount = +((rent_amount / days_in_month) * (days_in_month - yesterday.getDate())).toFixed(2)
+      }
+
+      format_currency('#customer_prorated_rent_amount', rent_amount)
+    });
+
+    $('#customer_deposit').on('change', function() {
+      format_currency('#customer_deposit_amount', +$(this).val())
+    });
+
+  }
 });
