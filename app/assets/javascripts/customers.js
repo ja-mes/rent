d187fd4f8b0c39994f49ea303538a9ab8bc52669
James <jsbrown000@gmail.com>
2016-06-23 18:50:21 -0500

    Display next payment date on payments

--- app/assets/javascripts/customers.js
+++ app/assets/javascripts/customers.js
@@ -10,6 +10,27 @@ $(document).on('turbolinks:load', function() {
     var yesterday = new Date(date.setDate(date.getDate() - 1));
     var days_in_month = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
 
+    function calculateDueDate() {
+      var due_date = $('#customer_due_date').val();
+      var dd = due_date;
+      var mm = date.getMonth()+2; //January is 0!
+
+      var yyyy = date.getFullYear();
+      if(dd<10){
+        dd='0'+dd
+      } 
+      if(mm<10){
+        mm='0'+mm
+      } 
+      var next_due_date = mm+'/'+dd+'/'+yyyy;
+
+      $('#customer_due_date_display').html(next_due_date)
+    }
+
+    $('#customer_due_date').on('change', calculateDueDate);
+    calculateDueDate();
+
+
     $('#customer_prorated_rent_checkbox').prop({ 'checked': true });
     $('#customer_deposit_checkbox').prop({ 'checked': true });
     $('#customer_days_in_month').html(days_in_month - yesterday.getDate());
