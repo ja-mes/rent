7fe33e666afd904795c013d9f19bbf700321ecd2
James <jsbrown000@gmail.com>
2016-06-29 11:38:39 -0500

    Update http verb syntax

--- test/controllers/vendors_controller_test.rb
+++ test/controllers/vendors_controller_test.rb
@@ -39,16 +39,16 @@ class VendorsControllerTest < ActionController::TestCase
     sign_in users(:one), scope: :user
 
     assert_difference 'Vendor.count' do
-      post :create, vendor: {
+      post :create, params: { vendor: {
         name: "Foo Bar"
-      }
+      }}
     end
 
     assert_equal assigns(:vendor).name, "Foo Bar"
   end
 
   test "post create should not work if the user is not logged in" do
-    post :create, vendor: {}
+    post :create, params: { vendor: {} }
     assert_redirected_to new_user_session_path
   end
 
@@ -57,20 +57,20 @@ class VendorsControllerTest < ActionController::TestCase
   test "show should work" do
     sign_in users(:one), scope: :user
 
-    get :show, id: vendors(:one)
+    get :show, params: { id: vendors(:one) }
     assert_template :show
     assert_response :success
     assert_not_nil assigns(:trans)
   end
 
   test "show should not work if the user is not logged in" do
-    get :show, id: vendors(:one)
+    get :show, params: { id: vendors(:one) }
     assert_redirected_to new_user_session_path
   end
 
   test "show should not work if the if the vendor does not belong to the user" do
     sign_in users(:user_without_properties), scope: :user
-    get :show, id: vendors(:one)
+    get :show, params: { id: vendors(:one) }
     assert_redirected_to root_path
   end
 
@@ -79,19 +79,19 @@ class VendorsControllerTest < ActionController::TestCase
   test "get edit" do
     sign_in users(:one), scope: :user
 
-    get :edit, id: vendors(:one)
+    get :edit, params: { id: vendors(:one) }
     assert_response :success
     assert_template :edit
   end
 
   test "get edit should not work if the user is not logged in" do
-    get :edit, id: vendors(:one)
+    get :edit, params: { id: vendors(:one) }
     assert_redirected_to new_user_session_path
   end
 
   test "get edit should not work if the vendor does not belong to the user" do
     sign_in users(:user_without_properties), scope: :user
-    get :edit, id: vendors(:one)
+    get :edit, params: { id: vendors(:one) }
     assert_redirected_to root_path
   end
 
@@ -100,21 +100,21 @@ class VendorsControllerTest < ActionController::TestCase
   test "put update" do
     sign_in users(:one), scope: :user
 
-    put :update, id: vendors(:one), vendor: {
+    put :update, params: { id: vendors(:one), vendor: {
       name: "A new name"
-    }
+    }}
 
     assert_equal assigns(:vendor).name, "A new name"
   end
 
   test "put update should not work if the user is not logged in" do
-    put :update, id: vendors(:one), vendor: {}
+    put :update, params: { id: vendors(:one), vendor: {} }
     assert_redirected_to new_user_session_path
   end
 
   test "put update should not work if the vendor does not belong to the user" do
     sign_in users(:user_without_properties), scope: :user
-    put :update, id: vendors(:one), vendor: {}
+    put :update, params: { id: vendors(:one), vendor: {} }
     assert_redirected_to root_path
   end
 end
