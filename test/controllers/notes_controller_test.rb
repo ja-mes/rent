fd9a6c9b7e68640fe7ecec945ceb95e33006a814
James <jsbrown000@gmail.com>
2016-06-29 08:31:20 -0500

    Update notes controller test for new http verb syntax

--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class NotesControllerTest < ActionController::TestCase
-  include Devise::TestHelpers
+  include Devise::Test::ControllerHelpers
   
   # GET index
   test "get index" do
-    sign_in :user, users(:one)
+    sign_in users(:one), scope: :user
 
-    get :index, customer_id: customers(:one)
+    get :index, params: { customer_id: customers(:one) }
     assert_response :success
     assert_template :index
 
@@ -16,48 +16,48 @@ class NotesControllerTest < ActionController::TestCase
   end
 
   test "edit should only work if the user is logged in" do
-    get :index, customer_id: customers(:one)
+    get :index, params: { customer_id: customers(:one) }
     assert_redirected_to new_user_session_path
   end
 
   test "get index should not work if the customer does not belong to the user" do
-    sign_in :user, users(:two)
+    sign_in users(:two), scope: :user
 
-    get :index, customer_id: customers(:one)
+    get :index, params: { customer_id: customers(:one) }
     assert_redirected_to root_path
   end
 
   
   # GET new
   test "get new" do
-    sign_in :user, users(:one)
+    sign_in users(:one), scope: :user
 
-    xhr :get, :new, customer_id: customers(:one)
+    get :new, xhr: true, params: { customer_id: customers(:one) }
     assert_response :success
     assert assigns(:note).new_record?
   end
 
   test "get new requires user" do
-    xhr :get, :new, customer_id: customers(:one)
+    get :new, xhr: true,  params: { customer_id: customers(:one) }
     assert_response :unauthorized
   end
 
   test "get new requires same customer user" do
-    sign_in :user, users(:user_without_properties)
-    xhr :get, :new, customer_id: customers(:one)
+    sign_in users(:user_without_properties), scope: :user
+    get :new, xhr: true, params: { customer_id: customers(:one) }
     assert_redirected_to root_path
   end
 
 
   # POST create
   test "post create" do
-    sign_in :user, users(:one)
+    sign_in users(:one), scope: :user
 
     assert_difference 'Note.count' do
-      xhr :post, :create, customer_id: customers(:one), note: {
+      post :create, xhr: true, params: { customer_id: customers(:one), note: {
         content: "foobar",
         date: "05/08/2016"
-      }
+      }}
     end
 
     assert_equal assigns(:note).content, "foobar"
@@ -65,53 +65,53 @@ class NotesControllerTest < ActionController::TestCase
   end
 
   test "post create requires user" do
-    xhr :post, :create, customer_id: customers(:one), note: {}
+    post :create, xhr: true, params: { customer_id: customers(:one), note: {} }
     assert_response :unauthorized
   end
 
   test "post create requires same customer user" do
-    sign_in :user, users(:user_without_properties)
-    post :create, customer_id: customers(:one), note: { content: "foobar" }
+    sign_in users(:user_without_properties), scope: :user
+    post :create, params: { customer_id: customers(:one), note: { content: "foobar" } }
     assert_redirected_to root_path
   end
 
 
   # GET edit
   test "get edit" do
-    sign_in :user, users(:one)
+    sign_in users(:one), scope: :user
 
-    xhr :get, :edit, customer_id: customers(:one), id: notes(:one)
+    get :edit, xhr: true, params: { customer_id: customers(:one), id: notes(:one) }
     assert_response :success
     assert_equal assigns(:note), notes(:one)
   end
 
   test "get edit requires user" do
-    xhr :get, :edit, customer_id: customers(:one), id: notes(:one)
+    get :edit, xhr: true, params: { customer_id: customers(:one), id: notes(:one) }
     assert_response :unauthorized
   end
 
   test "get edit requires customer user" do
-    sign_in :user, users(:user_without_properties)
-    xhr :get, :edit, customer_id: customers(:one), id: notes(:one)
+    sign_in users(:user_without_properties), scope: :user
+    get :edit, xhr: true, params: { customer_id: customers(:one), id: notes(:one) }
     assert_redirected_to root_path
   end
 
   test "get edit requires note user" do
-    sign_in :user, users(:one)
-    xhr :get, :edit, customer_id: customers(:one), id: notes(:two)
+    sign_in users(:one), scope: :user
+    get :edit, xhr: true, params: { customer_id: customers(:one), id: notes(:two) }
     assert_redirected_to root_path
   end
 
 
   # PUT update
   test "put update" do
-    sign_in :user, users(:one)
+    sign_in users(:one), scope: :user
 
     note = notes(:one)
-    xhr :put, :update, customer_id: customers(:one), id: note, note: {
+    put :update, xhr: true, params: { customer_id: customers(:one), id: note, note: {
       content: "the new content",
       date: "04/07/2016"
-    }
+    }}
 
     note.reload
     assert_equal note.content, "the new content"
@@ -119,29 +119,29 @@ class NotesControllerTest < ActionController::TestCase
   end
 
   test "put update requires user" do
-    xhr :put, :update, customer_id: customers(:one), id: notes(:one), note: {}
+    put :update, xhr: true, params: { customer_id: customers(:one), id: notes(:one), note: {} }
     assert_response :unauthorized
   end
 
   test "put update requires customer user" do
-    sign_in :user, users(:user_without_properties)
-    put :update, customer_id: customers(:one), id: notes(:one), note: {}
+    sign_in users(:user_without_properties), scope: :user
+    put :update, params: { customer_id: customers(:one), id: notes(:one), note: {} }
     assert_redirected_to root_path
   end
 
   test "put update requires note user" do
-    sign_in :user, users(:one)
-    put :update, customer_id: customers(:one), id: notes(:two), note: {}
+    sign_in users(:one), scope: :user
+    put :update, params: { customer_id: customers(:one), id: notes(:two), note: {} }
     assert_redirected_to root_path
   end
 
 
   # DELETE destroy
   test "delete destroy" do
-    sign_in :user, users(:one)
+    sign_in users(:one), scope: :user
     
     assert_difference 'Note.count', -1 do
-      xhr :delete, :destroy, customer_id: customers(:one), id: notes(:one)
+      delete :destroy, xhr: true, params: { customer_id: customers(:one), id: notes(:one) }
     end
 
     assert_not_nil assigns(:note)
@@ -149,27 +149,27 @@ class NotesControllerTest < ActionController::TestCase
 
   test "delete destroy requires logged in user" do
     assert_difference 'Note.count', 0 do
-      xhr :delete, :destroy, customer_id: customers(:one), id: notes(:one)
+      delete :destroy, xhr: true, params: { customer_id: customers(:one), id: notes(:one) }
     end
 
     assert_response :unauthorized
   end
 
   test "delete destroy requires same customer user" do
-    sign_in :user, users(:user_without_properties)
+    sign_in users(:user_without_properties), scope: :user
 
     assert_difference 'Note.count', 0 do
-      delete :destroy, customer_id: customers(:one), id: notes(:one)
+      delete :destroy, params: { customer_id: customers(:one), id: notes(:one) }
     end
 
     assert_redirected_to root_path
   end
 
   test "delete destroy requires same note user" do
-    sign_in :user, users(:one)
+    sign_in users(:one), scope: :user
 
     assert_difference 'Note.count', 0 do
-      delete :destroy, customer_id: customers(:one), id: notes(:two)
+      delete :destroy, params: { customer_id: customers(:one), id: notes(:two) }
     end
 
     assert_redirected_to root_path
