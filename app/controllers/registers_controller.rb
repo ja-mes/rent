2c073c3cd79c1af6d4f96a245350558d9edcc471
James <jsbrown000@gmail.com>
2016-06-29 17:05:13 -0500

    Eager load checks and deposits. Fixes #154

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -2,7 +2,7 @@ class RegistersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @trans = current_user.trans.where("transactionable_type = 'Deposit' OR transactionable_type = 'Check'").order('date DESC')
+    @trans = current_user.trans.includes(:transactionable).where("transactionable_type = 'Deposit' OR transactionable_type = 'Check'").order('date DESC')
     @balance = current_user.checkbook.balance
   end
 end
