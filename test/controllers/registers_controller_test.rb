01dd4a35b35017ed9339da71a9786058e9188d2b
James <jsbrown000@gmail.com>
2016-06-29 16:58:03 -0500

    Test registers controller

--- test/controllers/registers_controller_test.rb
+++ test/controllers/registers_controller_test.rb
@@ -2,4 +2,16 @@ require 'test_helper'
 
 class RegistersControllerTest < ActionController::TestCase
   include Devise::Test::ControllerHelpers
+
+  test "get index" do
+    sign_in users(:one), scope: :user
+
+    get :index
+    assert_response :success
+  end
+
+  test "get index should require logged in user" do
+    get :index
+    assert_redirected_to new_user_session_path
+  end
 end
