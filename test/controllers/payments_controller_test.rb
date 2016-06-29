c423fcc3212b2fc58f2857ffac742f8996c83a8e
James <jsbrown000@gmail.com>
2016-06-29 08:37:23 -0500

    Update payments controller test http verb syntax

--- test/controllers/payments_controller_test.rb
+++ test/controllers/payments_controller_test.rb
@@ -1,15 +1,17 @@
+require 'test_helper'
+
 class PaymentsControllerTest < ActionController::TestCase
   include Devise::Test::ControllerHelpers
 
   test "index should redirect to customer" do
     sign_in users(:one), scope: :user
-    get :index, customer_id: customers(:one)
+    get :index, params: { customer_id: customers(:one) }
     assert_redirected_to customers(:one)
   end
 
   test "get new" do
     sign_in users(:one), scope: :user
-    get :new, customer_id: customers(:one)
+    get :new, params: { customer_id: customers(:one) }
     assert_response :success
     assert_template :new
     assert_not_nil assigns(:customer)
@@ -19,13 +21,13 @@ class PaymentsControllerTest < ActionController::TestCase
 
   test "should only allow new if customer belongs to user" do
     sign_in users(:user_without_properties), scope: :user
-    get :new, customer_id: customers(:one)
+    get :new, params: { customer_id: customers(:one) }
     assert_redirected_to root_path
     assert_equal "You are not authorized to do that", flash[:danger]
   end
 
   test "new should redirect to sign in if no current user" do
-    get :new, customer_id: customers(:one)
+    get :new, params: { customer_id: customers(:one) }
     assert_redirected_to new_user_session_path
   end
 
@@ -35,14 +37,14 @@ class PaymentsControllerTest < ActionController::TestCase
     sign_in users(:one), scope: :user
 
     assert_difference ['Payment.count', 'Tran.count'], 1 do
-      post :create, customer_id: customers(:one), payment: {
+      post :create, params: { customer_id: customers(:one), payment: { 
         customer_id: customers(:one).id,
         method: "Money Order",
         amount: "300",
         date: "03/11/2016",
         memo: "hello world",
         num: "foo"
-      }
+      }}
     end
 
     assert_equal assigns(:payment).num, "foo"
@@ -58,11 +60,11 @@ class PaymentsControllerTest < ActionController::TestCase
     sign_in users(:one), scope: :user
 
     assert_difference 'Payment.count', 0 do
-      post :create, customer_id: customers(:one), payment: {
+      post :create, params: { customer_id: customers(:one), payment: { 
         amount: nil,
         date: nil,
         memo: " ",
-      }
+      }}
     end
 
     assert_template :new
@@ -71,19 +73,19 @@ class PaymentsControllerTest < ActionController::TestCase
 
   test "create should only work if customer belongs to user" do
     sign_in users(:user_without_properties), scope: :user
-    post :create, customer_id: customers(:one)
+    post :create, params: { customer_id: customers(:one) }
     assert_redirected_to root_path
     assert_equal "You are not authorized to do that", flash[:danger]
   end
 
   test "create should only work if user is signed in" do
-    post :create, customer_id: customers(:one)
+    post :create, params: { customer_id: customers(:one) }
     assert_redirected_to new_user_session_path
   end
 
   test "get edit" do
     sign_in users(:one), scope: :user
-    get :edit, customer_id: customers(:one), id: payments(:one)
+    get :edit, params: { customer_id: customers(:one), id: payments(:one) }
     assert_response :success
     assert_template :edit
     assert_not_nil assigns(:payment)
@@ -91,13 +93,13 @@ class PaymentsControllerTest < ActionController::TestCase
   end
 
   test "edit should only work if user is logged in" do
-    get :edit, customer_id: customers(:one), id: payments(:one)
+    get :edit, params: { customer_id: customers(:one), id: payments(:one) }
     assert_redirected_to new_user_session_path
   end
 
   test "edit should only work if customer belongs to user" do
     sign_in users(:user_without_properties), scope: :user
-    get :edit, customer_id: customers(:one), id: payments(:one)
+    get :edit, params: { customer_id: customers(:one), id: payments(:one) }
     assert_redirected_to root_path
     assert_equal "You are not authorized to do that", flash[:danger]
   end
@@ -106,13 +108,13 @@ class PaymentsControllerTest < ActionController::TestCase
 
   test "update should successfully update payment" do
     sign_in users(:one), scope: :user
-    put :update, customer_id: customers(:one), id: payments(:one), payment: {
+    put :update, params: { customer_id: customers(:one), id: payments(:one), payment: { 
       amount: "200",
       method: "Cash",
       date: "05/08/2016",
       memo: "blah memo",
       num: "5"
-    }
+    }}
 
     assert_equal assigns(:payment).num, "5"
     assert_equal assigns(:payment).amount, 200
@@ -128,11 +130,11 @@ class PaymentsControllerTest < ActionController::TestCase
 
     payments(:one).update_attribute :deposit, deposits(:one)
 
-    put :update, customer_id: customers(:one), id: payments(:one), payment: {
+    put :update, params: { customer_id: customers(:one), id: payments(:one), payment: { 
       amount: "500",
       date: "06/02/2016",
       memo: "foobar",
-    }
+    }}
 
     assert_not_equal assigns(:payment).amount, 500
     assert_not_equal assigns(:payment).date, "06/02/2016".to_date
@@ -143,39 +145,39 @@ class PaymentsControllerTest < ActionController::TestCase
   test "update should not save with invalid data" do
     sign_in users(:one), scope: :user
 
-    put :update, customer_id: customers(:one), id: payments(:one), payment: {
+    put :update, params: { customer_id: customers(:one), id: payments(:one), payment: { 
       amount: "",
       date: "",
       memo: "",
-    }
+    }}
 
     assert_template :edit
     assert_select '.form-errors'
   end
 
   test "update should not work if user is not logged in" do
-    put :update, customer_id: customers(:one), id: payments(:one)
+    put :update, params: { customer_id: customers(:one), id: payments(:one) }
     assert_redirected_to new_user_session_path
   end
 
   test "update should not work if customer does not belong to user" do
     sign_in users(:user_without_properties), scope: :user
 
-    put :update, customer_id: customers(:one), id: payments(:one)
+    put :update, params: { customer_id: customers(:one), id: payments(:one) }
     assert_redirected_to root_path
     assert_equal "You are not authorized to do that", flash[:danger]
   end
 
   test "show should redirect to customer" do
     sign_in users(:one), scope: :user
-    get :show, customer_id: customers(:one), id: payments(:one)
+    get :show, params: { customer_id: customers(:one), id: payments(:one) }
   end
 
   test "destroy should successfully destroy invoice" do
     sign_in users(:one), scope: :user
 
     assert_difference ['Payment.count', 'Tran.count'], -1 do
-      delete :destroy, customer_id: customers(:one), id: payments(:one)
+      delete :destroy, params: { customer_id: customers(:one), id: payments(:one) }
     end
 
     assert_not_nil assigns(:payment)
@@ -188,7 +190,7 @@ class PaymentsControllerTest < ActionController::TestCase
     payments(:one).update_attribute :deposit, deposits(:one)
 
     assert_difference ['Payment.count', 'Tran.count'], 0 do
-      delete :destroy, customer_id: customers(:one), id: payments(:one)
+      delete :destroy, params: { customer_id: customers(:one), id: payments(:one) }
     end
 
     assert_not_nil flash[:danger]
@@ -196,7 +198,7 @@ class PaymentsControllerTest < ActionController::TestCase
 
   test "destroy should not work if the user is not logged in" do
     assert_difference 'Payment.count', 0 do
-      delete :destroy, customer_id: customers(:one), id: payments(:one)
+      delete :destroy, params: { customer_id: customers(:one), id: payments(:one) }
     end
 
     assert_redirected_to new_user_session_path
@@ -206,7 +208,7 @@ class PaymentsControllerTest < ActionController::TestCase
     sign_in users(:user_without_properties), scope: :user
 
     assert_difference 'Payment.count', 0 do
-      delete :destroy, customer_id: customers(:one), id: payments(:one)
+      delete :destroy, params: { customer_id: customers(:one), id: payments(:one) }
     end
 
     assert_redirected_to root_path
@@ -216,7 +218,7 @@ class PaymentsControllerTest < ActionController::TestCase
     sign_in users(:one), scope: :user
 
     assert_difference 'Payment.count', 0 do
-      delete :destroy, customer_id: customers(:one), id: payments(:two)
+      delete :destroy, params: { customer_id: customers(:one), id: payments(:two) }
     end
 
     assert_redirected_to root_path
@@ -225,7 +227,7 @@ class PaymentsControllerTest < ActionController::TestCase
   test "receipt should work" do
     sign_in users(:one), scope: :user
 
-    get :receipt, customer_id: customers(:one), id: payments(:one)
+    get :receipt, params: { customer_id: customers(:one), id: payments(:one) }
 
     assert_response :success
     assert_not_nil assigns(:payment)
