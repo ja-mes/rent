1f2b71ce05ee0403cc883d0ae3b960f759bf5681
James <jsbrown000@gmail.com>
2016-06-30 13:50:17 -0500

    Test settings controller. Closes #150

--- test/controllers/settings_controller_test.rb
+++ test/controllers/settings_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
-class SettingsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class SettingsControllerTest < ActionController::TestCase
+  include Devise::Test::ControllerHelpers
+
+  test "get index" do
+    sign_in users(:one), scope: :user
+
+    get :index
+    assert_response :success
+  end
+
+  test "get index requires logged in user" do
+    get :index
+    assert_redirected_to new_user_session_path
+  end
+
+  test "post update_checkbook_balance" do
+    sign_in users(:one), scope: :user
+    checkbook = users(:one).checkbook
+    checkbook.balance = 0
+    checkbook.set_beginning_balance(500)
+
+    assert_equal checkbook.reload.balance, 500
+  end
 end
