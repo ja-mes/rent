27efd4da0c2149c20c276975aa1780ae08174a89
James <jsbrown000@gmail.com>
2016-06-30 09:27:32 -0500

    Add index action and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/settings', to: 'settings#index', as: 'settings'
   resources :registers
   resources :reconciliations
   resources :recurring_trans
