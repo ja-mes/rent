2b95e03646de3e2b72834dbf292ad67277d1cab0
James <jsbrown000@gmail.com>
2016-07-03 12:35:10 -0500

    Don't show comma if only address is present

--- app/models/property.rb
+++ app/models/property.rb
@@ -9,13 +9,18 @@ class Property < ApplicationRecord
   validates :state, length: { is: 2 }, allow_blank: true
   validates :rent, numericality: { greater_than_or_equal_to: 0, allow_blank: true }
   validates :deposit, numericality: { greater_than_or_equal_to: 0, allow_blank: true }
-  before_save :upcase_state
+  before_save :upcase_state, if: :state
 
   def upcase_state
     self.state = self.state.upcase
   end
 
   def full_address
-    "#{self.address}, #{self.city}, #{self.state} #{self.zip}"
+    only_address = self.city.blank? && self.state.blank? && self.zip.blank?
+    "#{self.address}#{',' unless only_address} #{self.city} #{self.state} #{self.zip}"
+  end
+
+  def self.create_all_properties_property(user_id)
+    self.create(user_id: user_id, address: "All Properties", internal: true)
   end
 end
