278737abc09a331e9e0c776f147f3af412e2ad24
James <jsbrown000@gmail.com>
2016-07-03 20:42:07 -0500

    Paginate check register. Closes #160

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -2,7 +2,7 @@ class RegistersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @trans = current_user.trans.includes(:transactionable).where("transactionable_type = 'Deposit' OR transactionable_type = 'Check'").order('date DESC')
+    @trans = current_user.trans.includes(:transactionable).where("transactionable_type = 'Deposit' OR transactionable_type = 'Check'").order('date DESC').paginate(page: params[:page])
     @balance = current_user.checkbook.balance
   end
 end
