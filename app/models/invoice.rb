44164818ac408c2d53095c674db085082898dc05
James <jsbrown000@gmail.com>
2016-07-05 11:59:11 -0500

    Move create tran and increment balance into their own methods

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,21 +1,24 @@
 class Invoice < ApplicationRecord
   attr_accessor :skip_tran_validation
 
+  # ASSOCIATIONS
   belongs_to :user
   belongs_to :customer
   has_one :tran, as: :transactionable, dependent: :destroy
 
-  # invoice trans
   has_many :account_trans, as: :account_transable, dependent: :destroy
   validates_presence_of :account_trans, unless: :skip_tran_validation
   accepts_nested_attributes_for :account_trans, allow_destroy: :true
 
-  # invoice validations
+  # VALIDATIONS
   validates :user_id, presence: true
   validates :customer_id, presence: true
   validates :amount, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than_or_equal_to: 0 }
   validates :date, presence: true
   validate :totals_must_equal, unless: :skip_tran_validation
+
+  # HOOKS
+  after_create :create_invoice_tran, :inc_balance
 
   def totals_must_equal
     amount = 0
@@ -30,17 +33,20 @@ class Invoice < ApplicationRecord
     end
   end
 
-  after_create do
-    self.create_tran(user: self.user, customer: self.customer, date: self.date)
-    self.customer.increment!(:balance, by = self.amount)
-  end
-
   after_update do
     self.tran.update_attributes(customer: self.customer, date: self.date)
   end
 
   after_destroy do
     self.customer.increment!(:balance, by = -self.amount) if self.customer
+  end
+
+  def create_invoice_tran
+    self.create_tran(user: self.user, customer: self.customer, date: self.date)
+  end
+  
+  def inc_balance
+    self.customer.increment!(:balance, by = self.amount)
   end
 
   def calculate_balance(old_amount, old_customer)
