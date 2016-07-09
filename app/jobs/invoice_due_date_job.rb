c4a20b7590b3f516f6fa4c8e7ab086d90ee2de10
James <jsbrown000@gmail.com>
2016-07-08 19:43:21 -0500

    Add perform method

--- app/jobs/invoice_due_date_job.rb
+++ app/jobs/invoice_due_date_job.rb
@@ -1,7 +1,11 @@
 class InvoiceDueDateJob < ApplicationJob
   queue_as :default
 
-  def perform(invoice_id)
-    Invoice.find(invoice_id).inc_balance
+  def perform(user_id)
+    user = User.find(user_id)
+
+    user.invoices.includes(:customer).where(due_date: Date.today, charged: false).find_each do |i|
+      customer.increment!(:balance, by = i.amount)
+    end
   end
 end
