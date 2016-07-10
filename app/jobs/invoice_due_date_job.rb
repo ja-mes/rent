75c7fc9034965d84afd8a8cd47b4d0c16b6b72d9
James <jsbrown000@gmail.com>
2016-07-10 07:28:42 -0500

    Show vendor in check register. Closes #168

--- app/jobs/invoice_due_date_job.rb
+++ app/jobs/invoice_due_date_job.rb
@@ -4,7 +4,7 @@ class InvoiceDueDateJob < ApplicationJob
   def perform(user_id)
     user = User.find(user_id)
 
-    user.invoices.includes(:customer).where(due_date: Date.today, charged: false).find_each do |i|
+    user.invoices.includes(:customer).where("due_date <= ? AND charged = false", Date.today).find_each do |i|
       i.customer.increment!(:balance, by = i.amount)
       i.update_attribute(:charged, false)
     end
