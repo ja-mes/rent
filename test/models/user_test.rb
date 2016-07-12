d7acc87e51b21e5040a511b4133a5379d394c5cb
James <jsbrown000@gmail.com>
2016-07-12 17:13:33 -0500

    Fix failing test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -32,7 +32,7 @@ class UserTest < ActiveSupport::TestCase
     end
 
     @user.account_types.destroy_all
-    assert_difference 'AccountType.count', 6 do
+    assert_difference 'AccountType.count', 7 do
       @user.create_default_accounts
     end
   end
