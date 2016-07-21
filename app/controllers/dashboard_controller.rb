555e53463d2a23b2315a814e43211c83d4fcb863
James <jsbrown000@gmail.com>
2016-07-21 13:20:25 -0500

    Calculate net income for each month

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,27 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
+    income = [0, 0, 0, 0, 0, 0]
+
+    # TODO move this to model
+    Invoice.where(date: 6.months.ago..Date.today).limit(3).find_each do |i|
+      0.upto(6) do |num|
+        if i.date <= num.months.ago
+          income[num] += i.amount.to_f
+        end
+      end
+    end
+
+    Check.where(date: 6.months.ago..Date.today).find_each do |i|
+      1.upto(6) do |num|
+        if i.date <= num.months.ago
+          income[num] -= i.amount.to_f
+        end
+      end
+    end
+
+    @income = income.reverse
   end
 
   def charge_rent
