9aefdaca7cb3a448fb398373b07445279f608a19
James <jsbrown000@gmail.com>
2016-07-26 13:14:53 -0500

    Refactor js. Closes #192

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,8 +1,39 @@
 $(document).on('turbolinks:load', function() {
   if ($('body')[0].className == 'dashboard index') {
-    var ctx = document.getElementById('myChart');
+    var netIncomeCtx = document.getElementById('netIncome');
+    var netIncomeChart = new Chart(netIncomeCtx, {
+      type: 'line',
+      data: {
+        labels: $('#income_months').val().split(','),
+        datasets: [
+          {
+            label: "Rental Income",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 2,
+            pointHoverRadius: 6,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 6,
+            pointHitRadius: 10,
+            data: $('#income').val().split(','),
+            spanGaps: false,
+          }
+        ]
+      }
+    });
 
-    var myChart = new Chart(ctx, {
+    var propertyCtx = document.getElementById('property');
+    var propertyChart = new Chart(propertyCtx, {
       type: 'doughnut',
       data: {
         labels: ['Rented properties', 'Vacant properties'],
@@ -21,43 +52,8 @@ $(document).on('turbolinks:load', function() {
       },
     });
 
-    var netIncomeCtx = document.getElementById('netIncome');
-
-    var data = {
-      labels: $('#income_months').val().split(','),
-      datasets: [
-        {
-          label: "Rental Income",
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: "rgba(75,192,192,1)",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 2,
-          pointHoverRadius: 6,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 6,
-          pointHitRadius: 10,
-          data: $('#income').val().split(','),
-          spanGaps: false,
-        }
-      ]
-    }
-
-    var myLineChart = new Chart(netIncomeCtx, {
-      type: 'line',
-      data: data,
-    });
 
     var expensesCtx = document.getElementById('expenses');
-
     var expensesChart = new Chart(expensesCtx, {
       type: 'bar',
       data: {
