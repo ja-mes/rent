e01c329e8c9e6248f55b9f975812a5e80088951f
James <jsbrown000@gmail.com>
2016-07-26 13:09:56 -0500

    Pull data from hidden field tags

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,10 +9,10 @@ class DashboardController < ApplicationController
       income[num] = trans.where(date: num.months.ago.beginning_of_month..num.months.ago.end_of_month).sum(:amount).to_f
     end
 
-    @income = income.reverse
+    @income = income.reverse.join(',')
     @income_months = 6.downto(1).map { |n| DateTime::MONTHNAMES.drop(1)[(Date.today.month - n) % 12] }.join(',')
 
-    @property_count = [current_user.properties.where(rented: true).count, current_user.properties.where(rented: false).count]
+    @property_count = [current_user.properties.where(rented: true).count, current_user.properties.where(rented: false).count].join(',')
 
     accounts = current_user.accounts.includes(:account_trans).where(account_type: current_user.expenses_account_type)
 
@@ -31,7 +31,7 @@ class DashboardController < ApplicationController
 
 
     accounts_top5 = accounts_hash.sort_by {|_key, value| -value}.take(5).to_h
-    @accounts_data = accounts_top5.values.collect {|i| i.to_f}
+    @accounts_data = accounts_top5.values.collect {|i| i.to_f}.join(',')
     @accounts_keys = accounts_top5.keys.take(5).join(',')
   end
 
